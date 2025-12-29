93c57815297affc9f2531dd9621f1ad4e43a10a2
Hesham Sayed <heshamsaed2003@hotmail.com>
2025-12-29 22:39:04 +0200

    App Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFFc49e85);
+  static const Color offWhite = Color(0xFFf8f4f9);
+  static const Color deepBrown = Color(0xFF684483E);
+  static const Color deepGrey = Color(0xFF6f6460);
+  static const Color Grey = Color(0xFFc4c4c4);
+  static const Color lightGrey = Color(0xFFb4b4b4);
+  static const Color black = Color(0xFF333333);
+}
